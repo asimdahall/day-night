131c057fd88fe48c27b68c568cd2cfb2f192ddb1
asimdahal <asim.dahal@joinsage.com>
2021-04-10 13:28:33 +0545

    feat: basic day design and animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as Cloud1 } from "./assets/cloud1.svg";
+import { ReactComponent as Cloud2 } from "./assets/cloud2.svg";
+import { ReactComponent as Hills } from "./assets/hills.svg";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="sky">
+      <div className="winds" id="winds1">
+        <div id="wind1" className="wind"></div>
+        <div id="wind2" className="wind"></div>
+        <div id="wind3" className="wind"></div>
+      </div>
+
+      <div className="winds">
+        <div id="wind1" className="wind"></div>
+        <div id="wind2" className="wind"></div>
+        <div id="wind3" className="wind"></div>
+      </div>
+      <div id="sun"></div>
+      <Cloud1 />
+      <Cloud2 />
+      <Hills />
     </div>
   );
 }

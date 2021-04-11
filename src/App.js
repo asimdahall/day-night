5e17fcb55176aeb218946715d24bdbb1b3f540cb
asimdahal <asim.dahal@joinsage.com>
2021-04-11 19:49:34 +0545

    feat: night mode integration

--- src/App.js
+++ src/App.js
@@ -1,27 +1,68 @@
 import { ReactComponent as Cloud1 } from "./assets/cloud1.svg";
 import { ReactComponent as Cloud2 } from "./assets/cloud2.svg";
 import { ReactComponent as Hills } from "./assets/hills.svg";
+import { ReactComponent as Moon } from "./assets/moon.svg";
 
 import "./App.css";
 
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const getNumberOfStars = () => getRandomArbitrary(100, 200);
+
+const getStarStyles = () => {
+  const size = getRandomArbitrary(2, 6) + "px";
+  return {
+    top: getRandomArbitrary(0, 80) + "vh",
+    left: getRandomArbitrary(0, 100) + "vw",
+    width: size,
+    height: size,
+  };
+};
+
+const numberOfStars = getNumberOfStars();
+
+const spawnStars = new Array(numberOfStars)
+  .fill(0)
+  .reduce((acc, cur, index) => {
+    return [
+      ...acc,
+      {
+        index,
+        ...getStarStyles(),
+      },
+    ];
+  }, []);
+
 function App() {
   return (
-    <div id="sky">
-      <div className="winds" id="winds1">
-        <div id="wind1" className="wind"></div>
-        <div id="wind2" className="wind"></div>
-        <div id="wind3" className="wind"></div>
-      </div>
+    <div>
+      <input id="toggler" type="checkbox" />
+      <div id="sky" className="day">
+        <div className="winds" id="winds1">
+          <div id="wind1" className="wind"></div>
+          <div id="wind2" className="wind"></div>
+          <div id="wind3" className="wind"></div>
+        </div>
 
-      <div className="winds">
-        <div id="wind1" className="wind"></div>
-        <div id="wind2" className="wind"></div>
-        <div id="wind3" className="wind"></div>
+        <div className="winds">
+          <div id="wind1" className="wind"></div>
+          <div id="wind2" className="wind"></div>
+          <div id="wind3" className="wind"></div>
+        </div>
+        <div id="sun"></div>
+        <div id="stars">
+          {spawnStars.map(({ index, ...star }) => (
+            <div key={index} id="star" style={star}></div>
+          ))}
+        </div>
+
+        <Cloud1 />
+        <Cloud2 />
+        <Hills />
+        <Moon />
       </div>
-      <div id="sun"></div>
-      <Cloud1 />
-      <Cloud2 />
-      <Hills />
     </div>
   );
 }
